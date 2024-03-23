257bb806b205bc081e31280ce21f761de9e08299
Andrei Shvedov <shvedov.4302@gmail.com>
2024-03-23 23:45:12 +0200

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ListViewGroup1 As System.Windows.Forms.ListViewGroup = New System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AccountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListViewMovies = New System.Windows.Forms.ListView()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Panel1.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.ComboBox3)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Location = New System.Drawing.Point(7, 27)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(110, 192)
+        Me.Panel1.TabIndex = 9
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(2, 30)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(105, 21)
+        Me.ComboBox3.TabIndex = 6
+        Me.ComboBox3.Text = "Filter By"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(2, 3)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(105, 21)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "Sort By"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.ViewToolStripMenuItem, Me.ListToolStripMenuItem, Me.AccountToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1032, 24)
+        Me.MenuStrip1.TabIndex = 12
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'ViewToolStripMenuItem
+        '
+        Me.ViewToolStripMenuItem.Name = "ViewToolStripMenuItem"
+        Me.ViewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.ViewToolStripMenuItem.Text = "View"
+        '
+        'ListToolStripMenuItem
+        '
+        Me.ListToolStripMenuItem.Name = "ListToolStripMenuItem"
+        Me.ListToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ListToolStripMenuItem.Text = "List"
+        '
+        'AccountToolStripMenuItem
+        '
+        Me.AccountToolStripMenuItem.Name = "AccountToolStripMenuItem"
+        Me.AccountToolStripMenuItem.Size = New System.Drawing.Size(64, 20)
+        Me.AccountToolStripMenuItem.Text = "Account"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.TextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        Me.TextBox1.Location = New System.Drawing.Point(306, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(493, 20)
+        Me.TextBox1.TabIndex = 11
+        Me.TextBox1.Text = "Film Otsing"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ListViewMovies
+        '
+        Me.ListViewMovies.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.ListViewMovies.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1})
+        ListViewGroup1.Header = "ListViewGroup"
+        ListViewGroup1.Name = "ListViewGroup1"
+        Me.ListViewMovies.Groups.AddRange(New System.Windows.Forms.ListViewGroup() {ListViewGroup1})
+        Me.ListViewMovies.HideSelection = False
+        Me.ListViewMovies.Location = New System.Drawing.Point(120, 27)
+        Me.ListViewMovies.Name = "ListViewMovies"
+        Me.ListViewMovies.Size = New System.Drawing.Size(881, 529)
+        Me.ListViewMovies.TabIndex = 10
+        Me.ListViewMovies.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(1032, 584)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ListViewMovies)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ViewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccountToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListViewMovies As ListView
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Drawing
+Imports Newtonsoft.Json
+Imports System.Windows.Forms
+Imports System.Net.Http
+Public Class Form1
+
+    Private WithEvents searchTimer As New System.Windows.Forms.Timer()
+
+    Public Sub New()
+        ' Этот вызов необходим конструктору Windows Forms.
+        InitializeComponent()
+
+        ' Инициализация searchTimer
+        searchTimer.Interval = 500 ' Задержка в миллисекундах
+    End Sub
+
+    ' Обработчик события тика таймера.
+    Private Async Sub searchTimer_Tick(sender As Object, e As EventArgs) Handles searchTimer.Tick
+        ' Останавливаем таймер
+        searchTimer.Stop()
+
+        ' Получаем поисковый запрос из TextBox
+        Dim searchQuery As String = TextBox1.Text
+        If Not String.IsNullOrWhiteSpace(searchQuery) Then
+            ' Вызываем функцию поиска фильмов асинхронно
+            Await SearchMoviesAsync(searchQuery)
+        Else
+            ' Если строка поиска пуста, очищаем ListView
+            ListViewMovies.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        ' Перезапуск таймера при каждом изменении текста
+        searchTimer.Stop()
+        searchTimer.Start()
+    End Sub
+
+    ' Асинхронная функция для поиска фильмов.
+    Public Async Function SearchMoviesAsync(searchQuery As String) As Task
+        Dim apiKey As String = "0d77f86880fc2d980da7ba1ab371bdbb" ' Ваш реальный API-ключ от TMDb
+        Dim requestUrl As String = $"https://api.themoviedb.org/3/search/movie?api_key={apiKey}&query={Uri.EscapeDataString(searchQuery)}"
+
+        Using httpClient As New HttpClient()
+            Dim response As String = Await httpClient.GetStringAsync(requestUrl)
+            Dim searchResults = JsonConvert.DeserializeObject(Of TmdbSearchResult)(response)
+
+            ' Теперь у вас есть searchResults с информацией о фильмах
+            If searchResults IsNot Nothing AndAlso searchResults.results.Count > 0 Then
+                ' Обновление элементов управления UI в потоке UI
+                If ListViewMovies.InvokeRequired Then
+                    ListViewMovies.Invoke(Sub() UpdateListView(searchResults))
+                Else
+                    UpdateListView(searchResults)
+                End If
+            End If
+        End Using
+    End Function
+
+    ' Метод для обновления ListView, вызываемый из SearchMoviesAsync
+    Private Sub UpdateListView(searchResults As TmdbSearchResult)
+        ' Создание ImageList и настройка ListView
+        Dim posters As New ImageList()
+        posters.ImageSize = New Size(100, 140) ' Примерный размер постеров
+        ' Создание словаря для жанров
+        Dim genreDictionary As New Dictionary(Of Integer, String)() ' Предполагаем, что у вас есть словарь ID жанров и их названий
+        ' Настройка столбцов ListView
+        ListViewMovies.Clear()
+        ListViewMovies.View = View.Details
+        ListViewMovies.Columns.Add("Название", 300)
+        ListViewMovies.Columns.Add("Год", 100)
+        ListViewMovies.Columns.Add("Жанр", 100)
+        ListViewMovies.Columns.Add("Рейтинг", 50)
+        ListViewMovies.SmallImageList = posters
+
+        ' Заполнение ListView элементами
+        For Each movie In searchResults.results
+            Dim item As New ListViewItem(movie.title)
+            Dim year As String = If(Not String.IsNullOrEmpty(movie.release_date) AndAlso movie.release_date.Length >= 4, movie.release_date.Substring(0, 4), "N/A")
+            Dim genreNames As New List(Of String)()
+
+            ' Получение имен жанров по ID
+            For Each genreId In movie.genre_ids
+                If genreDictionary.ContainsKey(genreId) Then
+                    genreNames.Add(genreDictionary(genreId))
+                End If
+            Next
+
+            Dim genre As String = String.Join(", ", genreNames) ' Соединяем имена жанров через запятую
+            Dim rating As String = movie.vote_average.ToString("0.0") ' Форматирование рейтинга до одного десятичного знака
+
+            item.SubItems.Add(year)
+            item.SubItems.Add(genre)
+            item.SubItems.Add(rating)
+
+            ' Загрузка изображения постера
+            Dim posterUrl As String = $"https://image.tmdb.org/t/p/w500{movie.poster_path}"
+            Dim poster As Image = LoadImageFromUrl(posterUrl) ' Убедитесь, что LoadImageFromUrl поддерживает синхронный запрос
+            If poster IsNot Nothing Then
+                posters.Images.Add(movie.id.ToString(), poster)
+                item.ImageKey = movie.id.ToString()
+            End If
+
+            ListViewMovies.Items.Add(item)
+        Next
+
+        ' Автоматическое изменение ширины столбцов для удобства просмотра
+        For i As Integer = 0 To ListViewMovies.Columns.Count - 1
+            ListViewMovies.Columns(i).Width = -2
+        Next
+    End Sub
+
+    Private Function LoadImageFromUrl(url As String) As Image
+        Try
+            Using client As New WebClient()
+                Using stream As Stream = client.OpenRead(url)
+                    Return Image.FromStream(stream)
+                End Using
+            End Using
+        Catch ex As Exception
+            ' Обработка ошибок загрузки изображения, например, можно вернуть Nothing или изображение-замещение
+            Return Nothing
+        End Try
+    End Function
+
+End Class
+
+
+' Классы для десериализации ответа API должны остаться без изменений.
+Public Class TmdbSearchResult
+    Public Property results As List(Of TmdbMovie)
+End Class
+
+Public Class TmdbMovie
+    Public Property title As String
+    Public Property release_date As String
+    Public Property genre_ids As List(Of Integer)
+    Public Property vote_average As Double
+    Public Property poster_path As String
+    Public Property id As Integer
+End Class
